9a0ecfdb36b4688c7cb4cae4aaf15ce74fd99162
yeuheni.vasiukovich@winfinity.live <yeuheni.vasiukovich@winfinity.live>
2024-12-04 14:17:14 +0300

    Two commit

---
+++ src/main/java/HomeWork1.java
@@ -0,0 +1,33 @@
+public class HomeWork1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+
+        byte v46 = 10;
+        short v47 = 25;
+        System.out.println(v46 + v47);
+
+
+        int v1 = 45;
+        byte v2 = 3;
+
+        float result = v1 / v2;
+
+        System.out.println(result);
+
+        double v3 = 45.32364;
+        float v4 = 10.23f;
+
+        double result1 = v3 % v4;
+
+        System.out.println(result1);
+
+        boolean relsult2 = (v47> v46) && (v1 % v2 == 0) && (v3 % v4 != 0);
+
+        System.out.println(relsult2);
+
+
+    }
+}
+
