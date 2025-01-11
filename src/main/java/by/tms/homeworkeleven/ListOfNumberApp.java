35f609b8ff8f19e33d0aed3ba97c8c5e6ad6c25d
yeuheni.vasiukovich@winfinity.live <yeuheni.vasiukovich@winfinity.live>
2025-01-11 15:07:59 +0300

    Home Work Eleven complite

---
+++ src/main/java/by/tms/homeworkeleven/ListOfNumberApp.java
@@ -0,0 +1,21 @@
+package by.tms.homeworkeleven;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ListOfNumberApp {
+    public static void main(String[] args) {
+        Integer[] listOfNumber = {1,2,2,3,4,5,6,8,8,6,4,2,9,9,3,5,6,8,7,5,2,3};
+        Map<Integer, Integer> set = new HashMap<>();
+
+        for (Integer num : listOfNumber){
+            set.put(num, set.getOrDefault(num, 0)+1);
+        }
+        System.out.println("Unique numbers and the number of their repetitions" + set);
+
+
+    }
+
+
+}
---
+++ src/main/java/by/tms/homeworkeleven/ListOfUserApp.java
@@ -0,0 +1,35 @@
+package by.tms.homeworkeleven;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ListOfUserApp {
+    public static void main(String[] args) {
+        Set<User> listOfUser = new HashSet<>();
+        listOfUser.add(new User("John Smith", 28));
+        listOfUser.add(new User("Emily Johnson", 34));
+        listOfUser.add(new User("Michael Brown", 22));
+        listOfUser.add(new User("Jessica Davis", 30));
+        listOfUser.add(new User("David Wilson", 25));
+        listOfUser.add(new User("David Wilson", 27));
+        listOfUser.add(new User("James Taylor", 31));
+        listOfUser.add(new User("Laura Anderson", 29));
+        listOfUser.add(new User("Robert Thomas", 33));
+        listOfUser.add(new User("Sophia Martinez", 26));
+
+        for (User i: listOfUser){
+            System.out.println(i.getName());
+        }
+
+        List<User> sortUserByAge = new ArrayList<>(listOfUser);
+        sortUserByAge.sort(new UserComparator());
+        System.out.println(sortUserByAge);
+
+
+
+
+    }
+}
